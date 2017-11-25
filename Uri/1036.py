2648119493edb23cfb5e668aa764679993823b0e
Kleber Barilli <rs05496321786@hotmail.com>
2017-11-25 01:18:51 +0000

    1

--- Uri/1036.py
+++ Uri/1036.py
@@ -1,30 +1,27 @@
 #Fórmula de Bháskara
 import math
-def errado():
-    while True:
-        print("Bem vindo ao resolvedor de Bháskara !!!")
+def calcula():
+
+    a = float(input("Digite o valor de a:"))
+    b = float(input("Digite o valor de b:"))
+    c = float(input("Digite o valor de c:"))
+    delta = (b**2) - 4*a*c
+    if delta <0:
+        print("Impossível calcular raiz negativa")
+        calcula()
+
+    else:
+        u = math.sqrt(delta)
+        x1 = (b + u) /2 * a
+        x2 = (b - u) /2 * a
+        print("x2 = ",x2)
+        print("x1 = ",x1)
+        opcao = input("Deseja calcular mais? [S/N]")
+
+        if opcao == "s" or  opcao == "S":
+            calcula()
 
 
-        try:
-            a = float(input("Digite o valor de a:"))
-            b = float(input("Digite o valor de b:"))
-            c = float(input("Digite o valor de c:"))
-            u = math.sqrt(b *b - 4 *a *c)
 
-            x1 = (b + u) /2 * a
-            x2 = (b - u) /2 * a
-            print("x2 = ",x2)
-            print("x1 = ",x1)
-            print("Se deseja sair digite 's' or 'S':\nOutra tecla para continuar")
-            opcao = input()
-            if opcao == "s" or  opcao == "S":
-                break
 
-        except ValueError:
-            print("Valores incorretetos")
-            print("Se deseja sair digite 's' or 'S':\nOutra tecla para continuar")
-            opcao = input()
-            if opcao == "s" or  opcao == "S":
-                break
-
-errado()
+calcula()
